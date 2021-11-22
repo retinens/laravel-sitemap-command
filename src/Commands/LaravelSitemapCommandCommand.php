a4f39b19a6a80b80244fdb733e76c69b58dbe6b9
WhereIsLucas <lucas@retinens.com>
2021-11-22 10:58:24 +0100

    wip

--- src/Commands/LaravelSitemapCommandCommand.php
+++ src/Commands/LaravelSitemapCommandCommand.php
@@ -15,7 +15,6 @@ class LaravelSitemapCommandCommand extends Command
     {
         if (strlen(config('sitemap-command.website_url'))) {
             SitemapGenerator::create(config('sitemap-command.website_url'))->writeToFile(public_path('/sitemap.xml'));
-
             return self::SUCCESS;
         } else {
             $this->error('Please setup an valid APP_URL in your .env file');
